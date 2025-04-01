f68316576c4992f7cc9ca178631d3d47d5098429
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2025-04-01 07:35:20 +0200

    resttemplate test

---
+++ src/main/java/ipeters/resttemplate_demo/configuration/SubscriberApplication.java
@@ -0,0 +1,21 @@
+package ipeters.resttemplate_demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+import java.util.logging.Filter;
+
+//@Configuration
+public class SubscriberApplication {
+
+//    @Bean
+//    public Filter logFilter() {
+//        CommonsRequestLoggingFilter filter = new
+//                CommonsRequestLoggingFilter();
+//        filter.setIncludeQueryString(true);
+//        filter.setIncludePayload(true);
+//        filter.setMaxPayloadLength(5120);
+//        return filter;
+//    }
+}
